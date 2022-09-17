b9729f8d51bb6ac22ed0f7f5d48825e67949e761
lilly <lilly1987@naver.com>
2022-09-17 14:36:12 +0900

    ...

--- FLERP/lilly.cs
+++ FLERP/lilly.cs
@@ -23,9 +23,10 @@ namespace FLERP
         private ConfigEntry<bool> isGUIOn;
         private ConfigEntry<bool> isOpen;
         private ConfigEntry<float> uiW;
+        private ConfigEntry<float> uiH;
 
         public int windowId = 542;
-        public Rect WindowRect;
+        public Rect windowRect;
 
         public string windowName = "";
         public string FullName = "Plugin";
@@ -46,6 +47,18 @@ namespace FLERP
         public static ConfigEntry<bool> removeFromShop;
         public static ConfigEntry<bool> customShop;
 
+        public static ConfigEntry<bool> eMultOn;
+
+        public static ConfigEntry<float> eHealthMult;
+        public static ConfigEntry<float> eArmorMult;
+        public static ConfigEntry<float> eDamageMult;
+        public static ConfigEntry<float> eSpeedMult;
+        
+        public static ConfigEntry<float> eHealthAdd;
+        public static ConfigEntry<float> eArmorAdd;
+        public static ConfigEntry<float> eDamageAdd;
+        public static ConfigEntry<float> eSpeedAdd;
+
         public void Awake()
         {
             logger = Logger;
@@ -57,42 +70,59 @@ namespace FLERP
             isOpen = Config.Bind("GUI", "isOpen", true);
             isOpen.SettingChanged += IsOpen_SettingChanged;
             uiW = Config.Bind("GUI", "uiW", 300f);
+            uiH = Config.Bind("GUI", "uiH", 600f);
 
             if (isOpen.Value)
-                WindowRect = new Rect(Screen.width - 65, 0, uiW.Value, 800);
+                windowRect = new Rect(Screen.width - 65, 0, uiW.Value, 800);
             else
-                WindowRect = new Rect(Screen.width - uiW.Value, 0, uiW.Value, 800);
+                windowRect = new Rect(Screen.width - uiW.Value, 0, uiW.Value, 800);
 
             IsOpen_SettingChanged(null, null);
 
             // BuildGadgetMenu.rerollCost : int @0400067A
             //buildGadgetMenu = AccessTools.TypeByName("BuildGadgetMenu");
             buildGadgetMenu = typeof(BuildGadgetMenu);
+            
             rerollCost = Config.Bind("Game", "rerollCost", 1);
             rerollCostItem = Config.Bind("Game", "rerollCostItem", 4);
             baseGadgetCount = Config.Bind("Game", "baseGadgetCount", 8);
-            addGainXP = Config.Bind("Game", "addGainXP", 9);
-            pickupRadius = Config.Bind("Game", "pickupRadius", 50f);
             removeFromShop = Config.Bind("Game", "removeFromShop", false);
             customShop = Config.Bind("Game", "customShop", true);
+            
+            
+            pickupRadius = Config.Bind("Game", "pickupRadius", 50f);
+            addGainXP = Config.Bind("Game", "addGainXP", 9);
+
+            eMultOn = Config.Bind("Game", "eMultOn", false);
+            //eHealthMult = Config.Bind("Game", "eMult", 2f);
+
+            eHealthMult = Config.Bind("Game", "eHealthMult", 2f);
+            eArmorMult = Config.Bind("Game", "eArmorMult", 2f);
+            eDamageMult = Config.Bind("Game", "eDamageMult", 2f);
+            eSpeedMult = Config.Bind("Game", "eSpeedMult", 2f);
+            
+            eHealthAdd = Config.Bind("Game", "eHealthAdd", 1f);
+            eArmorAdd = Config.Bind("Game", "eArmorAdd", 1f);
+            eDamageAdd = Config.Bind("Game", "eDamageAdd", 1f);
+            eSpeedAdd = Config.Bind("Game", "eSpeedAdd", 1f);
         }
 
         public void IsOpen_SettingChanged(object sender, EventArgs e)
         {
-            logger.LogInfo($"IsOpen_SettingChanged {isOpen.Value} , {isGUIOn.Value},{WindowRect.x} ");
+            logger.LogInfo($"IsOpen_SettingChanged {isOpen.Value} , {isGUIOn.Value},{windowRect.x} ");
             if (isOpen.Value)
             {
-                h = GUILayout.Height(800);
+                h = GUILayout.Height(uiH.Value);
                 w = GUILayout.Width(uiW.Value);
                 windowName = FullName;
-                WindowRect.x -= (uiW.Value - 64);
+                windowRect.x -= (uiW.Value - 64);
             }
             else
             {
                 h = GUILayout.Height(40);
                 w = GUILayout.Width(60);
                 windowName = ShortName;
-                WindowRect.x += (uiW.Value - 64);
+                windowRect.x += (uiW.Value - 64);
             }
         }
 
@@ -118,7 +148,9 @@ namespace FLERP
             if (!isGUIOn.Value)
                 return;
 
-            WindowRect = GUILayout.Window(windowId, WindowRect, WindowFunction, windowName, w, h);
+            windowRect.x = Mathf.Clamp(windowRect.x, -windowRect.width + 4, Screen.width - 4);
+            windowRect.y = Mathf.Clamp(windowRect.y, -windowRect.height + 4, Screen.height - 4);
+            windowRect = GUILayout.Window(windowId, windowRect, WindowFunction, windowName, w, h);
         }
 
         public virtual void WindowFunction(int id)
@@ -155,8 +187,8 @@ namespace FLERP
                 GUILayout.BeginHorizontal();
                 GUILayout.Label($"Gadget rerollCost : {rerollCost.Value}");
                 GUILayout.FlexibleSpace();
-                if (GUILayout.Button("<", GUILayout.Width(20), GUILayout.Height(20))) { AccessTools.Field(buildGadgetMenu, "rerollCost").SetValue(BuildGadgetMenu.instance, --rerollCost.Value); }
-                if (GUILayout.Button(">", GUILayout.Width(20), GUILayout.Height(20))) { AccessTools.Field(buildGadgetMenu, "rerollCost").SetValue(BuildGadgetMenu.instance, ++rerollCost.Value); }
+                if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { AccessTools.Field(buildGadgetMenu, "rerollCost").SetValue(BuildGadgetMenu.instance, --rerollCost.Value); }
+                if (GUILayout.Button("+", GUILayout.Width(20), GUILayout.Height(20))) { AccessTools.Field(buildGadgetMenu, "rerollCost").SetValue(BuildGadgetMenu.instance, ++rerollCost.Value); }
                 GUILayout.EndHorizontal();
                 //             
                 if (GadgetManager.instance != null)
@@ -164,16 +196,16 @@ namespace FLERP
                     GUILayout.BeginHorizontal();
                     GUILayout.Label($"MaxGadgetCount : {GadgetManager.instance.MaxGadgetCount}");
                     GUILayout.FlexibleSpace();
-                    if (GUILayout.Button("<", GUILayout.Width(20), GUILayout.Height(20))) { GadgetManager.instance.MaxGadgetCount--; }
-                    if (GUILayout.Button(">", GUILayout.Width(20), GUILayout.Height(20))) { GadgetManager.instance.MaxGadgetCount++; }
+                    if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { GadgetManager.instance.MaxGadgetCount--; }
+                    if (GUILayout.Button("+", GUILayout.Width(20), GUILayout.Height(20))) { GadgetManager.instance.MaxGadgetCount++; }
                     GUILayout.EndHorizontal();
                 }
                 //
                 GUILayout.BeginHorizontal();
                 GUILayout.Label($"baseGadgetCount : {baseGadgetCount.Value}");
                 GUILayout.FlexibleSpace();
-                if (GUILayout.Button("<", GUILayout.Width(20), GUILayout.Height(20))) { baseGadgetCount.Value--; }
-                if (GUILayout.Button(">", GUILayout.Width(20), GUILayout.Height(20))) { baseGadgetCount.Value++; }
+                if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { baseGadgetCount.Value--; }
+                if (GUILayout.Button("+", GUILayout.Width(20), GUILayout.Height(20))) { baseGadgetCount.Value++; }
                 GUILayout.EndHorizontal();
                 //
 
@@ -182,8 +214,8 @@ namespace FLERP
                 GUILayout.BeginHorizontal();
                 GUILayout.Label($"Item rerollCost -= : {rerollCostItem.Value}");
                 GUILayout.FlexibleSpace();
-                if (GUILayout.Button("<", GUILayout.Width(20), GUILayout.Height(20))) { rerollCostItem.Value--; }
-                if (GUILayout.Button(">", GUILayout.Width(20), GUILayout.Height(20))) { rerollCostItem.Value++; }
+                if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { rerollCostItem.Value--; }
+                if (GUILayout.Button("+", GUILayout.Width(20), GUILayout.Height(20))) { rerollCostItem.Value++; }
                 GUILayout.EndHorizontal();
                 //
 
@@ -192,18 +224,116 @@ namespace FLERP
                 GUILayout.BeginHorizontal();
                 GUILayout.Label($"pickupRadius : {pickupRadius.Value}");
                 GUILayout.FlexibleSpace();
-                if (GUILayout.Button("<", GUILayout.Width(20), GUILayout.Height(20))) { pickupRadius.Value -= 5f; }
-                if (GUILayout.Button(">", GUILayout.Width(20), GUILayout.Height(20))) { pickupRadius.Value += 5f; }
+                if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { pickupRadius.Value -= 5f; }
+                if (GUILayout.Button("+", GUILayout.Width(20), GUILayout.Height(20))) { pickupRadius.Value += 5f; }
                 GUILayout.EndHorizontal();
                 //
 
                 GUILayout.BeginHorizontal();
                 GUILayout.Label($"addGainXP : {addGainXP.Value}");
                 GUILayout.FlexibleSpace();
-                if (GUILayout.Button("<", GUILayout.Width(20), GUILayout.Height(20))) { addGainXP.Value--; }
-                if (GUILayout.Button(">", GUILayout.Width(20), GUILayout.Height(20))) { addGainXP.Value++; }
+                if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { addGainXP.Value--; }
+                if (GUILayout.Button("+", GUILayout.Width(20), GUILayout.Height(20))) { addGainXP.Value++; }
                 GUILayout.EndHorizontal();
                 //
+
+                GUILayout.Label("=== Enemy ===");
+                               
+                                
+                GUILayout.Label("--- when Spawn ---");
+                GUILayout.Label("HP=HealthMult * (1f + 0.8f * currNgBuffRatio)*eHealthMult");
+                if (GUILayout.Button($"Mult apply: {eMultOn.Value}")) { eMultOn.Value = !eMultOn.Value; }
+                GUILayout.BeginHorizontal();
+                GUILayout.Label($"eHealthMult : {eHealthMult.Value}");
+                GUILayout.FlexibleSpace();
+                if (GUILayout.Button("1", GUILayout.Width(20), GUILayout.Height(20))) { eHealthMult.Value = 1f; }
+                if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { eHealthMult.Value -= 0.1f; }
+                if (GUILayout.Button("+", GUILayout.Width(20), GUILayout.Height(20))) { eHealthMult.Value += 0.1f; }
+                GUILayout.EndHorizontal();
+                GUILayout.BeginHorizontal();
+                GUILayout.Label($"eArmorMult : {eArmorMult.Value}");
+                GUILayout.FlexibleSpace();
+                if (GUILayout.Button("1", GUILayout.Width(20), GUILayout.Height(20))) { eArmorMult.Value = 1f; }
+                if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { eArmorMult.Value -= 0.1f; }
+                if (GUILayout.Button("+", GUILayout.Width(20), GUILayout.Height(20))) { eArmorMult.Value += 0.1f; }
+                GUILayout.EndHorizontal();
+                GUILayout.BeginHorizontal();
+                GUILayout.Label($"eDamageMult : {eDamageMult.Value}");
+                GUILayout.FlexibleSpace();
+                if (GUILayout.Button("1", GUILayout.Width(20), GUILayout.Height(20))) { eDamageMult.Value = 1f; }
+                if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { eDamageMult.Value -= 0.1f; }
+                if (GUILayout.Button("+", GUILayout.Width(20), GUILayout.Height(20))) { eDamageMult.Value += 0.1f; }
+                GUILayout.EndHorizontal();
+                GUILayout.BeginHorizontal();
+                GUILayout.Label($"eSpeedMult : {eSpeedMult.Value}");
+                GUILayout.FlexibleSpace();
+                if (GUILayout.Button("1", GUILayout.Width(20), GUILayout.Height(20))) { eSpeedMult.Value = 1f; }
+                if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { eSpeedMult.Value -= 0.1f; }
+                if (GUILayout.Button("+", GUILayout.Width(20), GUILayout.Height(20))) { eSpeedMult.Value += 0.1f; }
+                GUILayout.EndHorizontal();
+
+                GUILayout.Label("--- edit property ---");
+                GUILayout.Label("HealthMult reset when game start");
+                GUILayout.Label("HealthMult-=eHealthAdd");
+                //GUILayout.Label("HealthMult+=eHealthAdd");
+                //GUILayout.BeginHorizontal();
+                //GUILayout.Label($"eHealthAdd : {eHealthAdd.Value}");
+                //GUILayout.FlexibleSpace();
+                //if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { eHealthAdd.Value -= 0.1f; }
+                //if (GUILayout.Button("+", GUILayout.Width(20), GUILayout.Height(20))) { eHealthAdd.Value += 0.1f; }
+                //GUILayout.EndHorizontal();
+                //GUILayout.BeginHorizontal();
+                //GUILayout.Label($"eArmorAdd : {eArmorAdd.Value}");
+                //GUILayout.FlexibleSpace();
+                //if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { eArmorAdd.Value -= 0.1f; }
+                //if (GUILayout.Button("+", GUILayout.Width(20), GUILayout.Height(20))) { eArmorAdd.Value += 0.1f; }
+                //GUILayout.EndHorizontal();
+                //GUILayout.BeginHorizontal();
+                //GUILayout.Label($"eDamageAdd : {eDamageAdd.Value}");
+                //GUILayout.FlexibleSpace();
+                //if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { eDamageAdd.Value -= 0.1f; }
+                //if (GUILayout.Button("+", GUILayout.Width(20), GUILayout.Height(20))) { eDamageAdd.Value += 0.1f; }
+                //GUILayout.EndHorizontal();
+                //GUILayout.BeginHorizontal();
+                //GUILayout.Label($"eSpeedAdd : {eSpeedAdd.Value}");
+                //GUILayout.FlexibleSpace();
+                //if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { eSpeedAdd.Value -= 0.1f; }
+                //if (GUILayout.Button("+", GUILayout.Width(20), GUILayout.Height(20))) { eSpeedAdd.Value += 0.1f; }
+                //GUILayout.EndHorizontal();
+
+                if (EnemySpawner.instance != null)
+                {
+
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label($"HealthMult : {EnemySpawner.instance.HealthMult}");
+                    GUILayout.FlexibleSpace();
+                    if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { EnemySpawner.instance.HealthMult -= eHealthAdd.Value; }
+                    if (GUILayout.Button("+", GUILayout.Width(20), GUILayout.Height(20))) { EnemySpawner.instance.HealthMult += eHealthAdd.Value; }
+                    GUILayout.EndHorizontal();
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label($"ArmorMult : {EnemySpawner.instance.ArmorMult}");
+                    GUILayout.FlexibleSpace();
+                    if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { EnemySpawner.instance.HealthMult -= eArmorAdd.Value ; }
+                    if (GUILayout.Button("+", GUILayout.Width(20), GUILayout.Height(20))) { EnemySpawner.instance.HealthMult += eArmorAdd.Value ; }
+                    GUILayout.EndHorizontal();
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label($"DamageMult : {EnemySpawner.instance.DamageMult}");
+                    GUILayout.FlexibleSpace();
+                    if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { EnemySpawner.instance.HealthMult -= eDamageAdd.Value ; }
+                    if (GUILayout.Button("+", GUILayout.Width(20), GUILayout.Height(20))) { EnemySpawner.instance.HealthMult += eDamageAdd.Value ; }
+                    GUILayout.EndHorizontal();
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label($"SpeedMult : {EnemySpawner.instance.SpeedMult}");
+                    GUILayout.FlexibleSpace();
+                    if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { EnemySpawner.instance.HealthMult -= eSpeedAdd.Value ; }
+                    if (GUILayout.Button("+", GUILayout.Width(20), GUILayout.Height(20))) { EnemySpawner.instance.HealthMult += eSpeedAdd.Value ; }
+                    GUILayout.EndHorizontal();
+
+                }
+                else
+                {
+                    GUILayout.Label("EnemySpawner is bull");
+                }
 
                 GUILayout.EndScrollView();
             }
@@ -328,7 +458,70 @@ namespace FLERP
                 ___currXP += addGainXP.Value;
             }
         }
+        /*
+        [HarmonyPatch(typeof(EnemySpawner), "SpawnEnemy")]
+        [HarmonyPostfix]
+        public static void HealthMult(ref List<GameObject> __result, float ___currNgBuffRatio)
+        {
+            if (!eMultOn.Value)
+            {
+                return ;
+            }
+            foreach (GameObject gameObject in __result)
+            {
+                gameObject.GetComponent<HealthManager>().SetMaxHealthModifier(EnemySpawner.instance.HealthMult * (1f + 0.8f * ___currNgBuffRatio)* eHealthMult.Value);
+                gameObject.GetComponent<HealthUnit>().ArmorMult*=eArmorMult.Value;
+                gameObject.GetComponent<Steerer>().MoveSpeedMult*=eSpeedMult.Value;
+                gameObject.GetComponent<AEnemy>().DamageMult*=eDamageMult.Value;
+            }
+        }
+        */
 
+        [HarmonyPatch(typeof(HealthManager), "SetMaxHealthModifier")]
+        [HarmonyPrefix]
+        public static void SetMaxHealthModifier(ref float modifier)
+        {
+            if (!eMultOn.Value)
+            {
+                return;
+            }
+            modifier *= eHealthMult.Value;
+        }
+        
+
+        [HarmonyPatch(typeof(HealthUnit), "ArmorMult",MethodType.Setter)]
+        [HarmonyPrefix]
+        public static void SetArmorMult(ref float __0)
+        {
+            if (!eMultOn.Value)
+            {
+                return;
+            }
+            __0 *= eArmorMult.Value;
+        }
+        
+
+        [HarmonyPatch(typeof(Steerer), "MoveSpeedMult", MethodType.Setter)]
+        [HarmonyPrefix]
+        public static void SetMoveSpeedMult(ref float __0)
+        {
+            if (!eMultOn.Value)
+            {
+                return;
+            }
+            __0 *= eSpeedMult.Value;
+        }
+        
+        [HarmonyPatch(typeof(AEnemy), "DamageMult", MethodType.Setter)]
+        [HarmonyPrefix]
+        public static void SetDamageMult(ref float __0)
+        {
+            if (!eMultOn.Value)
+            {
+                return;
+            }
+            __0 *= eDamageMult.Value;
+        }
 
     }
 }
